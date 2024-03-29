e2b3e2a3322fe5921f417489c3353dd194f6994a
MackKJ <k.mackjr2002@gmail.com>
2024-03-28 23:35:46 -0500

    Add to cart
    Completed the add to cart functionality as well as changed html files to extend the base.html

--- bookaholics/cart/cart.py
+++ bookaholics/cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Inventory
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -8,3 +10,20 @@ class Cart():
             cart = self.session['sessionKey'] = {}
 
         self.cart = cart
+
+    def add(self, book):
+        book_isbn = str(book.isbn)
+
+        if book_isbn in self.cart:
+            pass
+        else:
+            self.cart[book_isbn] = {'price': str(book.price)}
+
+        self.session.modified = True
+
+    def get_cart(self):
+        book_isbns = self.cart.keys()
+
+        books = Inventory.objects.filter(isbn__in=book_isbns)
+
+        return books
