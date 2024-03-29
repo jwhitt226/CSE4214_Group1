df35a05d53d9680c8537492fdc115f60c7b03fb1
LANDYNSULLI <landynsullivan@icloud.com>
2024-03-28 21:04:54 -0500

    Added search functionallity

--- bookaholics/store/views/listing.py
+++ bookaholics/store/views/listing.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from ..models import Inventory
+from django.db.models import Q
 
 def browse(request):
-
-    books = Inventory.objects.all()
-
+    if 'q' in request.GET:
+        q = request.GET['q']
+        multiple_q = Q(Q(title__icontains = q) | Q(author__icontains = q) | Q(isbn__icontains = q))
+        books = Inventory.objects.filter(multiple_q)
+    else:
+        books = Inventory.objects.all()
     return render(request, 'userPage.html', {'books': books})
+
+
 
 
 def viewListing(request, pk):
