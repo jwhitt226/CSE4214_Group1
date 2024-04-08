d7b7ff74b0069ef20e5c5f46494c1e24039bfbd3
LANDYNSULLI <landynsullivan@icloud.com>
2024-04-08 15:28:18 -0500

    Added the browse by genre fuctionality also altered database and added one nonfiction book

--- bookaholics/store/views/listing.py
+++ bookaholics/store/views/listing.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 def browse(request):
     if 'q' in request.GET:
         q = request.GET['q']
-        multiple_q = Q(Q(title__icontains = q) | Q(author__icontains = q) | Q(isbn__icontains = q))
+        multiple_q = Q(Q(title__icontains = q) | Q(author__icontains = q) | Q(isbn__icontains = q) | Q(genre = q.lower()))
         books = Inventory.objects.filter(multiple_q)
     else:
         books = Inventory.objects.all()
