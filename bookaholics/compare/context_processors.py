eda1a7bdc2b774c441d7095536999cbf0599106a
LANDYNSULLI <landynsullivan@icloud.com>
2024-04-10 16:43:12 -0500

    half of compare_branch complete

---
+++ bookaholics/compare/context_processors.py
@@ -0,0 +1,4 @@
+from .compare import Compare
+
+def compare(request):
+    return {'compare': Compare(request)}
---
+++ bookaholics/compare/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.compare, name="compare"),
+    path('add/', views.compareAdd, name="compareAdd"),
+    path('delete/', views.compareDelete, name="compareDelete"),
+    path('update/', views.compareUpdate, name="compareUpdate"),
+]
